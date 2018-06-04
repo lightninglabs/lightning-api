baee6bf10fba846fe24c33a3dfa19f3e674761ea
Wilmer Paulino <wilmer.paulino@gmail.com>
2018-06-04 15:37:02 -0700

    modify script to also update lnd+lncli

--- update_and_render.sh
+++ update_and_render.sh
@@ -5,7 +5,16 @@ curl -o rpc.proto -s https://raw.githubusercontent.com/lightningnetwork/lnd/mast
 # Generate the rpc.json file from rpc.proto, so that generate_slate_docs.py can parse it
 protoc -I. -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --doc_out=json,rpc.json:. rpc.proto
 
-# Generate the new docs
+# Update lncli to the respective commit.
+pushd $GOPATH/src/github.com/lightningnetwork/lnd
+git reset --hard HEAD
+git pull
+git checkout $commit
+make
+make install
+popd
+
+# Render the new docs.
 ./render.py
 
 # Clean up
